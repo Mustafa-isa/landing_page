ce426e653ba2b3f07678b228604b306cc40ca321
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-10-20 19:03:09 +0300

    ...

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './*.html',  // Include root-level HTML files
+    './*.{js,ts}',  // Include root-level JS or TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
